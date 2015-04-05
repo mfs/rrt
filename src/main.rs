eaa24f67186c4750dfe7dcdfe93e2c2caf78d5b8
Mike Sampson <mike@sambodata.com>
2015-04-05 12:57:01 +1000

    Output TGA file instead of PNG.
    Image crate still being fixed post beta. Switched to outputting a TGA. Should
    add RLE at a minimum and put this into a crate.

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * */
 
-extern crate image;
+extern crate byteorder;
 
 mod vector;
 mod ray;
@@ -21,18 +21,20 @@ mod geometry;
 
 use std::fs::File;
 use std::path::Path;
+use std::io::Write;
 use vector::Vector;
 use ray::Ray;
 use geometry::Sphere;
 use geometry::Geometry;
 use geometry::Triangle;
+use byteorder::{LittleEndian, WriteBytesExt};
 
 fn main() {
 
    let imgx = 500;
    let imgy = 500;
 
-   let mut img = image::ImageBuffer::new(imgx, imgy);
+   let mut img = vec![(0, 0, 0); imgx * imgy];
 
    let dir = Vector::new(0.0, 0.0, -1.0);
    let s = Sphere::new(250.0, 250.0, -1000.0, 150.0);
@@ -49,7 +51,7 @@ fn main() {
    for y in (0 .. imgx) {
       for x in (0 .. imgy) {
          let mut tmax = 100000.0;
-         let mut color = [0, 0, 0];
+         let mut color = (0, 0, 0);
          let r = Ray { origin: Vector::new(x as f32, y as f32, 0.0), direction: dir };
 
          for sh in shapes.iter() {
@@ -57,19 +59,28 @@ fn main() {
                None => {},
                Some(hr) => {
                   tmax = hr.t;
-                  color = [hr.color.x as u8, hr.color.y as u8, hr.color.z as u8]
+                  color = (hr.color.x as u8, hr.color.y as u8, hr.color.z as u8);
                },
             }
          }
-
-         img.put_pixel(x, imgy - y - 1, image::Rgb(color));
+         img[x + imgx * (imgy - y - 1)] = color;
       }
    }
 
-   let ref mut fout = File::create(&Path::new("trace.png")).unwrap();
-   match image::ImageRgb8(img).save(fout, image::PNG) {
-      Ok(_) => (),
-      Err(err) => {println!("{}", err)},
+   // Write file out as a 24 bit uncompressed TGA.
+   // http://en.wikipedia.org/wiki/Truevision_TGA
+   let ref mut fout = File::create(&Path::new("trace.tga")).unwrap();
+   // field 1,2,3,4
+   fout.write(&[0, 0, 2, 0, 0, 0, 0, 0]).unwrap();
+   // field 5
+   fout.write_u16::<LittleEndian>(0 as u16).unwrap();
+   fout.write_u16::<LittleEndian>(0 as u16).unwrap();
+   fout.write_u16::<LittleEndian>(imgx as u16).unwrap();
+   fout.write_u16::<LittleEndian>(imgy as u16).unwrap();
+   fout.write(&[24, 32]).unwrap();
+   // image data
+   for pix in img {
+      let (p0, p1, p2) = pix;
+      fout.write(&[p0, p1, p2]).unwrap();
    }
-
 }
